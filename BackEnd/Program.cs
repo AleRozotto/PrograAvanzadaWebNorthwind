f5a9d151dd809178c80290c596a15b71c400aea7
Alessandro Rozotto <alerozotto@gmail.com>
2024-03-24 22:32:31 -0600

    Use APIK

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -1,3 +1,4 @@
+using BackEnd.Middleware;
 using BackEnd.Services.Implementations;
 using BackEnd.Services.Interfaces;
 using DAL.Implementations;
@@ -38,6 +39,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ApiKeyManager>();
 
 app.UseAuthorization();
 
